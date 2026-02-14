ce17f461d7576e8e2b3b6fcb4224d83af855f937
nikitachoudhary114 <nikitachoudhary364@gmail.com>
2026-02-14 21:11:59 +0530

    model & prompts

--- langchain-models/1.LLMs/1_llm_demo.py
+++ langchain-models/1.LLMs/1_llm_demo.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = OpenAI(model = 'gpt-3.5-turbo-instruct')
+llm = OpenAI(model='gpt-3.5-turbo-instruct')
 
-result = llm.invoke("what is capitla of india")
+result = llm.invoke("What is the capital of India")
 
 print(result)
